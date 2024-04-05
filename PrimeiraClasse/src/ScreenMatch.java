5e56237b08ed1f871c17db54a5e4cba0b2d83f23
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-04 22:45:29 -0300

    Atribuindo valores ao filme

---
+++ PrimeiraClasse/src/ScreenMatch.java
@@ -0,0 +1,17 @@
+public class ScreenMatch {
+    public static void main(String[] args){
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoLancamento);
+
+        boolean incluidoPlano = false;
+        System.out.println("Incluído no plano? " + incluidoPlano);
+
+        double notaFilme = 8.1F;
+        System.out.println("Nota: " + notaFilme);
+        
+
+    }
+}
