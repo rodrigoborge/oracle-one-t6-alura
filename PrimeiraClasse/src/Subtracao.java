7127644a8b79ad2f49bb9905eb1da5fe07c55d22
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-08 21:15:12 -0300

    atualizacao variavel subtracao

--- PrimeiraClasse/src/Subtracao.java
+++ PrimeiraClasse/src/Subtracao.java
@@ -1,7 +1,7 @@
 public class Subtracao {
 
     public static void main(String [] args) {
-        int Subtracao = 10 - 5;
-        System.out.println(Subtracao);
+        int subtracao = 10 - 5;
+        System.out.println(subtracao);
     }
 }
