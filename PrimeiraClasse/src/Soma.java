94ec6f8d89908e75b9ba5aa05af7a718c0b05eca
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-08 21:14:56 -0300

    atualizacao variavel soma

--- PrimeiraClasse/src/Soma.java
+++ PrimeiraClasse/src/Soma.java
@@ -1,7 +1,7 @@
 public class Soma {
 
     public static void main(String[] args){
-        int Soma = 10 + 5;
-        System.out.println(Soma);
+        int soma = 10 + 5;
+        System.out.println(soma);
     }
 }
