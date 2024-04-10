44ca9b7052b70ec13951a32a931d43fd65a4b8e2
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-09 21:08:39 -0300

    Formato de string

---
+++ PrimeiraClasse/src/FormadoString.java
@@ -0,0 +1,10 @@
+public class FormadoString {
+    public static void main(String[] args){
+
+        String nome = "Maria";
+        int idade = 30;
+        double valor = 55.9999;
+        System.out.println(String.format("Meu nome é %s, eu tenho %d anos e hoje gastei %.2f reais", nome, idade, valor));
+
+    }
+}
