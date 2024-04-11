4ef1ead8f7a1d2b50d6b73a9454fcca19a32dc58
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-11 20:46:36 -0300

    Desafio aula 2: Valor em dolares

---
+++ PrimeiraClasse/src/DesafioValorDolares.java
@@ -0,0 +1,8 @@
+public class DesafioValorDolares {
+    public static void main  (String [] args){
+        double valorEmDolares = 25;
+        double valorEmReais = 4.94;
+        double conversao = valorEmDolares * valorEmReais;
+        System.out.printf("O valor de %.0f convertido em reais é de %.0f", valorEmDolares, conversao);
+    }
+}
