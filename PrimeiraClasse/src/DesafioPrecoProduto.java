42a8085eea205ebe2439987907781f07aa246c96
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-11 20:46:47 -0300

    Desafio aula 2: Preço de Produto

---
+++ PrimeiraClasse/src/DesafioPrecoProduto.java
@@ -0,0 +1,8 @@
+public class DesafioPrecoProduto {
+    public static void main (String [] args){
+        double precoProduto  = 345;
+        int quantidade = 230;
+        double valorTotal = precoProduto * quantidade;
+        System.out.println("O valor total é de: " + valorTotal);
+    }
+}
