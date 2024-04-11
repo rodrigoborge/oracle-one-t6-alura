6639c51bef4ab80add326f1ffe7295b20d5128ff
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-11 20:53:34 -0300

    Desafio aula 2: exercicio média

--- PrimeiraClasse/src/DesafioMedia.java
+++ PrimeiraClasse/src/DesafioMedia.java
@@ -3,8 +3,8 @@ public class DesafioMedia {
         int numero1 = 30;
         int numero2 = 20;
 
-        double media = (numero1 + numero2) / 2;
+        double media = (numero1 + numero2) / 2.0;
 
-        System.out.println("A média dos dois numeros é de: "+media);
+        System.out.println("A média dos dois numeros é de: " + media);
     }
 }
