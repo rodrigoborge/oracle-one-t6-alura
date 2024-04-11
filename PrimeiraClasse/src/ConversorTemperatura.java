c62fad0108049d5d1850240900268c1c24d666cb
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-11 20:48:44 -0300

    Desafio aula 2: Conversor de temperatura

---
+++ PrimeiraClasse/src/ConversorTemperatura.java
@@ -0,0 +1,7 @@
+public class ConversorTemperatura {
+    public static void main (String []args){
+        int temperatura = 30;
+        double conversor = (temperatura * 1.8) + 32;
+        System.out.println("A temperatura em celcius é de: "+ conversor);
+    }
+}
