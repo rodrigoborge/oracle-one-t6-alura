452acce61ba33a3c7774ae4d386b814ab54c777c
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-11 20:47:22 -0300

    Desafio aula 2: Concatenaçao de letra com palavra

---
+++ PrimeiraClasse/src/DesafioConcat.java
@@ -0,0 +1,8 @@
+public class DesafioConcat {
+    public static void main(String[]args){
+        char primeiraLetra = 82;
+        String restoNome = "odrigo";
+        System.out.println(primeiraLetra + restoNome);
+
+    }
+}
