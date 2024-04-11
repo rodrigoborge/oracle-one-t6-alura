d2c8a9f145ac9cc0f73f065d69192580c3cb8bff
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-11 20:50:40 -0300

    Desafio aula 2: atualizaçao soma valor1 + valor2 com casting para  int

--- PrimeiraClasse/src/DesafioCasting.java
+++ PrimeiraClasse/src/DesafioCasting.java
@@ -2,7 +2,7 @@ public class DesafioCasting {
     public static void main (String [] args){
         double valor1 = 23.4;
         int valor2 = 10;
-        int result = (int) valor1;
+        int result = valor2 + (int) valor1;
         System.out.println(result);
     }
 }
