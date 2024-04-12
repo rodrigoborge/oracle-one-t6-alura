ccaa92811be104c1db39ee45f15f8ddb844e3495
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-12 19:31:15 -0300

    Aula 04: Leitura de dados teclado

--- PrimeiraClasse/src/Leitura.java
+++ PrimeiraClasse/src/Leitura.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class Leitura {
     public static void main(String[] args) {
+
         Scanner leitura = new Scanner(System.in);
+
         System.out.println("Digite seu filme favorito: ");
         String filmeFavorito = leitura.nextLine();
+
         System.out.println("Qual o ano de lançamento? ");
         int anoLancamento = leitura.nextInt();
+
+        System.out.println("De 0 a 5, como você avalia este filme?");
+        double avaliacaoFilme = leitura.nextDouble();
+
         System.out.println("Seu filme favorito é: " + filmeFavorito);
         System.out.println("O ano de lançamento do filme é: " + anoLancamento);
+        System.out.println("Sua avaliaçao foi de: " + avaliacaoFilme);
 
     }
 }
