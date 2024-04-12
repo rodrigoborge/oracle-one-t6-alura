106fa709dae2039a2627557061c7d0995dc317f1
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-12 19:23:31 -0300

    Aula 04: Dia da Semana com switch case

---
+++ PrimeiraClasse/src/DiaSemana.java
@@ -0,0 +1,34 @@
+public class DiaSemana {
+    public static void main(String[] args) {
+        int dia = 3;
+        String nomeDia;
+
+        switch (dia) {
+            case 1:
+                nomeDia = "Domingo";
+                break;
+            case 2:
+                nomeDia = "Segunda";
+                break;
+            case 3:
+                nomeDia = "Terça";
+                break;
+            case 4:
+                nomeDia = "Quarta";
+                break;
+            case 5:
+                nomeDia = "Quinta";
+                break;
+            case 6:
+                nomeDia = "Sexta";
+                break;
+            case 7:
+                nomeDia = "Sábado";
+                break;
+            default:
+                nomeDia = "Valor inválido";
+                break;
+        }
+        System.out.println("O dia " + dia + " é " + nomeDia);
+    }
+}
