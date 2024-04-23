f518a47a9393895678541382aa69751ac54fd092
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-23 20:51:07 -0300

    Crie uma classe Aluno com atributos nome, idade, e um método para exibir informações. Crie uma instância da classe Aluno, atribua valores aos seus atributos e utilize o método para exibir as informações.

---
+++ Exercicios/src/Aluno.java
@@ -0,0 +1,10 @@
+public class Aluno {
+    String nome;
+    int idade;
+
+    boolean exibeAluno(){
+        System.out.println("Nome do Aluno: " + nome);
+        System.out.println("Idade do Aluno: " + idade);
+        return false;
+    }
+}
---
+++ Exercicios/src/PrincipalAluno.java
@@ -0,0 +1,9 @@
+public class PrincipalAluno {
+    public static void main(String[] args) {
+        Aluno fichaAluno = new Aluno();
+        fichaAluno.nome = "Rodrigo Borge";
+        fichaAluno.idade = 34;
+
+        fichaAluno.exibeAluno();
+    }
+}
