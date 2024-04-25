3a8bd9b6025aa677bcad93eb49fc433ec035b821
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-24 21:37:48 -0300

    Crie uma classe ContaBancaria com os seguintes atributos: numeroConta (privado), saldo (privado) e titular (publico). Implemente métodos getters e setters para os atributos privados.

---
+++ Exercicios/src/aula02/poo/exercicios/ContaBancaria.java
@@ -0,0 +1,31 @@
+package aula02.poo.exercicios;
+
+public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+    public String titularConta;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitularConta() {
+        return titularConta;
+    }
+
+    public void setTitularConta(String titularConta) {
+        this.titularConta = titularConta;
+    }
+}
