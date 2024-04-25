e96188451a685f97fe2108aef639c7eb56d447e5
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-24 21:01:09 -0300

    Organizando o código

--- ScreenMatch/src/screenmatch/modelos/Filme.java
+++ ScreenMatch/src/screenmatch/modelos/Filme.java
@@ -11,16 +11,49 @@ public class Filme {
         //Métodos
 
         //Usamos o GET quando queremos pegar um valor, assim como usamos set quando queremos atribuir um valor.
-        public int getTotalAvaliacoes(){
-                return totalAvaliacoes;
+
+        public String getNome() {
+                return nome;
         }
 
         public void setNome(String nome) {
                 this.nome = nome;
         }
 
+        public int getAnoLancamento() {
+                return anoLancamento;
+        }
+
         public void setAnoLancamento(int anoLancamento) {
                 this.anoLancamento = anoLancamento;
+        }
+
+        public boolean isIncluidoPlano() {
+                return incluidoPlano;
+        }
+
+        public void setIncluidoPlano(boolean incluidoPlano) {
+                this.incluidoPlano = incluidoPlano;
+        }
+
+        public double getSomaAvaliacao() {
+                return somaAvaliacao;
+        }
+
+        public void setSomaAvaliacao(double somaAvaliacao) {
+                this.somaAvaliacao = somaAvaliacao;
+        }
+
+        public int getTotalAvaliacoes() {
+                return totalAvaliacoes;
+        }
+
+        public void setTotalAvaliacoes(int totalAvaliacoes) {
+                this.totalAvaliacoes = totalAvaliacoes;
+        }
+
+        public int getDuracaoFilme() {
+                return duracaoFilme;
         }
 
         public void setDuracaoFilme(int duracaoFilme) {
