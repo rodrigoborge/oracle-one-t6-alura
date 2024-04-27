e82d072b2c7bf44a39139b048ea941bfbf6ca4b9
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-26 23:01:34 -0300

    Aproveitando modelos

--- ScreenMatch/src/screenmatch/modelos/Filme.java
+++ ScreenMatch/src/screenmatch/modelos/Filme.java
@@ -1,78 +1,14 @@
 package screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoLancamento;
-    private boolean incluidoPlano;
-    private double somaAvaliacao;
-    private int totalAvaliacoes;
-    private int duracaoFilme;
+public class Filme extends Titulo {
+    private String diretor;
 
-    //Métodos
 
-    //Usamos o GET quando queremos pegar um valor, assim como usamos set quando queremos atribuir um valor.
-
-    public String getNome() {
-        return nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAnoLancamento() {
-        return anoLancamento;
-    }
-
-    public void setAnoLancamento(int anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
-
-    public boolean isIncluidoPlano() {
-        return incluidoPlano;
-    }
-
-    public void setIncluidoPlano(boolean incluidoPlano) {
-        this.incluidoPlano = incluidoPlano;
-    }
-
-    public double getSomaAvaliacao() {
-        return somaAvaliacao;
-    }
-
-    public void setSomaAvaliacao(double somaAvaliacao) {
-        this.somaAvaliacao = somaAvaliacao;
-    }
-
-    public int getTotalAvaliacoes() {
-        return totalAvaliacoes;
-    }
-
-    public void setTotalAvaliacoes(int totalAvaliacoes) {
-        this.totalAvaliacoes = totalAvaliacoes;
-    }
-
-    public int getDuracaoFilme() {
-        return duracaoFilme;
-    }
-
-    public void setDuracaoFilme(int duracaoFilme) {
-        this.duracaoFilme = duracaoFilme;
-    }
-
-    public void exibeFichaTecnica() {
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoLancamento);
-        System.out.println("Incluído no plano: " + incluidoPlano);
-        System.out.println("Duração do filme: " + duracaoFilme);
-    }
-
-    public void avaliaFilme(double nota) {
-        somaAvaliacao += nota;
-        totalAvaliacoes++;
-    }
-
-    public double mediaAvaliacoes() {
-        return somaAvaliacao / totalAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
