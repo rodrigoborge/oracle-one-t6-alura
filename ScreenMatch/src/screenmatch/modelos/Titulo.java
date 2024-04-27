84d835790fffa4275cf09b43a174a65e386d5fda
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-26 23:01:46 -0300

    Aproveitando modelos

---
+++ ScreenMatch/src/screenmatch/modelos/Titulo.java
@@ -0,0 +1,74 @@
+package screenmatch.modelos;
+
+public class Titulo {
+    private String nome;
+    private int anoLancamento;
+    private boolean incluidoPlano;
+    private double somaAvaliacao;
+    private int totalAvaliacoes;
+    private int duracaoFilme;
+
+    //Métodos
+
+    //Usamos o GET quando queremos pegar um valor, assim como usamos set quando queremos atribuir um valor.
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public boolean isIncluidoPlano() {return incluidoPlano;}
+
+    public void setIncluidoPlano(boolean incluidoPlano) {
+        this.incluidoPlano = incluidoPlano;
+    }
+
+    public double getSomaAvaliacao() {
+        return somaAvaliacao;
+    }
+
+    public void setSomaAvaliacao(double somaAvaliacao) {
+        this.somaAvaliacao = somaAvaliacao;
+    }
+
+    public int getTotalAvaliacoes() {
+        return totalAvaliacoes;
+    }
+
+    public void setTotalAvaliacoes(int totalAvaliacoes) {
+        this.totalAvaliacoes = totalAvaliacoes;
+    }
+
+    public int getDuracaoFilme() {
+        return duracaoFilme;
+    }
+
+    public void setDuracaoFilme(int duracaoFilme) {
+        this.duracaoFilme = duracaoFilme;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoLancamento);
+    }
+
+    public void avaliaFilme(double nota) {
+        somaAvaliacao += nota;
+        totalAvaliacoes++;
+    }
+
+    public double mediaAvaliacoes() {
+        return somaAvaliacao / totalAvaliacoes;
+    }
+}
