dc720c51b86f261bca9050d7b77dfaf17cd9cff8
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-28 23:26:26 -0300

    Desafio: hora da prática - 03

---
+++ Exercicios/src/aula03/poo/exercicios/TesteContaBancaria.java
@@ -0,0 +1,18 @@
+package aula03.poo.exercicios;
+
+public class TesteContaBancaria {
+    public static void main(String[] args) {
+        // Criando uma conta bancária com saldo inicial de 1000
+        ContaBancaria conta = new ContaBancaria();
+        conta.depositar(1000);
+        conta.consultarSaldo();
+
+        // Realizando operações na conta corrente
+        ContaCorrente contaCorrente = new ContaCorrente();
+        contaCorrente.depositar(200);
+        contaCorrente.cobrarTarifaMensal();
+        contaCorrente.consultarSaldo();
+        contaCorrente.sacar(150);
+        contaCorrente.consultarSaldo();
+    }
+}
