eaa6e9748f68085e353e13d9f831d61cf3ba12dc
Rodrigo Borge <rodrigo@borge.com.br>
2024-04-28 22:59:11 -0300

    Evitando código duplicado

---
+++ ScreenMatch/src/screenmatch/modelos/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,26 @@
+package screenmatch.modelos.calculos;
+//import screenmatch.modelos.Filme;
+//import screenmatch.modelos.Serie;
+import screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+//    public void inclui(Filme f){
+//        tempoTotal += f.getDuracaoFilme();
+//    }
+//
+//    public void inclui(Serie s){
+//        tempoTotal += s.getDuracaoFilme();
+//    }
+
+    public void inclui(Titulo titulo){
+        System.out.println("Adicionando duração em minutos de: " + titulo);
+        this.tempoTotal  += titulo.getDuracaoFilme();
+    }
+
+}
