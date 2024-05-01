0358e85fb787960c8b8331cb21200ea2df06ef44
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-01 18:21:15 -0300

    Criando métodos reutilizáveis

---
+++ MinhasMusicas/src/br/principal/alura/minhasmusicas/principal/Principal.java
@@ -0,0 +1,38 @@
+package br.principal.alura.minhasmusicas.principal;
+
+import br.principal.alura.minhasmusicas.modelos.MinhasPreferidas;
+import br.principal.alura.minhasmusicas.modelos.Musica;
+import br.principal.alura.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("Bolha DEV");
+        meuPodcast.setApresentador("Marcos Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+    }
+}
