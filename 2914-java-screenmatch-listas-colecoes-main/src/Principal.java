0d87728a3ff2333e502dfc1fa28600a55af82822
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-02 20:31:25 -0300

    01 - Coleção de filmes

--- 2914-java-screenmatch-listas-colecoes-main/src/Principal.java
+++ 2914-java-screenmatch-listas-colecoes-main/src/Principal.java
@@ -4,6 +4,9 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -50,5 +53,24 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        var filmeDoPaulo = new Filme();
+        filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        filmeDoPaulo.setAnoDeLancamento(2003);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("O tamando da lista de filmes é de: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println("To String do filme: " + listaDeFilmes.get(0).toString());
+        System.out.println(listaDeFilmes);
+
+
+
+
     }
 }
