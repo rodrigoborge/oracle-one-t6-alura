5416a5db150cfbd322d51c5a65fdc5c49630ac6a
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-02 20:31:19 -0300

    01 - Coleção de filmes

---
+++ Exercicios/src/aula1/colecao/de/dados/lista/exercicios1/Pessoa.java
@@ -0,0 +1,27 @@
+package aula1.colecao.de.dados.lista.exercicios1;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.nome + " Idade: " + this.idade;
+    }
+}
