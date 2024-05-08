439b65354c970ed02a371224795566d6c558d602
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-07 22:02:57 -0300

    02 -   Transformando dados

--- 2914-java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ 2914-java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -22,5 +22,7 @@ public class PrincipalComBusca {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+
+
     }
 }
