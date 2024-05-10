0d9cf7e3852041bbd2aba2ce6f4707c497384553
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-09 23:10:03 -0300

    Desafio - Busca CEP por API

---
+++ BuscaCEP/src/ConsultaCep.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.Gson;
+
+public class ConsultaCep {
+    public Endereco buscaEndereco(String cep) throws RuntimeException {
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco)
+                .build();
+
+        try {
+            HttpResponse<String> response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
+            throw new RuntimeException("CEP não encontrado.");
+        }
+
+
+    }
+}
