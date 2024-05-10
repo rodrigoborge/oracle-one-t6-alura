d5f39d5caac352c428039efddf85f78d4dba71bb
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-09 23:10:15 -0300

    Desafio - Busca CEP por API

--- BuscaCEP/src/Principal.java
+++ BuscaCEP/src/Principal.java
@@ -1,2 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
+
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+
+        System.out.println("Digite o número do CEP (apenas numeros): ");
+        var cep = leitura.nextLine();
+
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorArquivo gerador = new GeradorArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+    }
 }
