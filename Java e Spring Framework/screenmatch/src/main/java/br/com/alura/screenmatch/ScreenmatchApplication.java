e6bf380eb5382bc885789fca96ae9e7c364645cc
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-26 20:50:26 -0300

    Modelando episódios

--- Java e Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Java e Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.services.ConsumoApi;
 import br.com.alura.screenmatch.services.ConverteDados;
@@ -17,10 +18,17 @@ public class ScreenmatchApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         var consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=4a00064d");
+        var json = consumoApi.obterDados("https://www.omdbapi" +
+                ".com/?t=gilmore+girls&apikey=4a00064d");
         System.out.println(json);
         ConverteDados conversor = new ConverteDados();
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
+        json = consumoApi.obterDados("https://omdbapi" +
+                ".com/?t=gilmore+girls&season=1&episode=2&apikey=4a00064d");
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json,
+                DadosEpisodio.class);
+        System.out.println(dadosEpisodio);
+
     }
 }
