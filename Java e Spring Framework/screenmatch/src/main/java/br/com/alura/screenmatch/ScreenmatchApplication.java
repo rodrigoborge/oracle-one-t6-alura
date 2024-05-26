c7ea38e359b11443c5e611cb8f12f28c048011ec
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-26 20:31:21 -0300

    Modelando dados da série

--- Java e Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Java e Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.services.ConsumoApi;
+import br.com.alura.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,5 +19,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
         var consumoApi = new ConsumoApi();
         var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=4a00064d");
         System.out.println(json);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
     }
 }
