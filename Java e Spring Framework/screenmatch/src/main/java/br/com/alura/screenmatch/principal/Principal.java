3a95cd7d9d168bb656c88851ae9fd641dc2f088a
Rodrigo Borge <rodrigo@borge.com.br>
2024-05-27 21:23:30 -0300

    Identificando os top 5 melhores episódios

--- Java e Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ Java e Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -6,9 +6,8 @@ import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.services.ConsumoApi;
 import br.com.alura.screenmatch.services.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -44,5 +43,17 @@ public class Principal {
 //        }
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTOP 5 EPISÓDIOS:");
+
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
